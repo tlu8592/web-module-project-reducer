2b57fdc2db50f78ab93f5eb54d58a3941f17a4c1
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-10 16:38:10 -0500

    connected a premade action

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,15 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- The action creator dispatched within the event handler gets called.
+- Then comes the reducer.
+- Since the action.type is ADD_ONE, the return logic for the switch case gets executed
+  return({
+  ...state,
+  total: state.total + 1
+  });
+  ...
+
+- TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import reducer from './reducers/index';
 import { initialState } from './reducers/index';
+import { addOne } from './actions/index';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
@@ -8,6 +9,10 @@ import CalcButton from './components/CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleAddOne = () => {
+    dispatch(addOne())
+  }
 
   return (
     <div className="App">
@@ -32,7 +37,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton onClick={handleAddOne} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
