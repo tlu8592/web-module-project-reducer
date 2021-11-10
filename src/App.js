fbfa5cd1bea653db89995883792f1f6902d945b5
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-10 16:48:02 -0500

    connected a better premade action

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 import reducer from './reducers/index';
 import { initialState } from './reducers/index';
-import { addOne } from './actions/index';
+import { addOne, applyNumber } from './actions/index';
 import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
@@ -10,8 +10,12 @@ import CalcButton from './components/CalcButton';
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handleAddOne = () => {
-    dispatch(addOne())
+  // const handleAddOne = () => {
+  //   dispatch(addOne())
+  // }
+
+  const handleApplyNumber = (num) => {
+    dispatch(applyNumber(num));
   }
 
   return (
@@ -37,7 +41,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handleAddOne} value={1}/>
+              <CalcButton onClick={() => handleApplyNumber(1)} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
